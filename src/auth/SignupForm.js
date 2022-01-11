f26761b187bc4c6b3036bbb0a7485f9137581817
davegrnr <davegrnr@gmail.com>
2022-01-10 20:34:20 -0500

    More debug

--- src/auth/SignupForm.js
+++ src/auth/SignupForm.js
@@ -26,12 +26,12 @@ function SignupForm({ signup }) {
     });
     const [formErrors, setFormErrors] = useState([]);
 
-    console.debug(
-        "SignupForm",
-        "signup=", typeof signup,
-        "formData=", formData,
-        "formErrors=", formErrors,
-    );
+    // console.debug(
+    //     "SignupForm",
+    //     "signup=", typeof signup,
+    //     "formData=", formData,
+    //     "formErrors=", formErrors,
+    // );
 
     /** Handle form submit:
    *
